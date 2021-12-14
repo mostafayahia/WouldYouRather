d6be85d5ed3f826eaf69a8978d55f0f2ab6f6f62
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2021-12-14 12:21:42 +0200

    starter code

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+import {
+    _getUsers, _getQuestions, _saveQuestion, _saveQuestionAnswer
+} from './_DATA';
+
+export function getUsers() {
+    return _getUsers();
+}
+
+export function getQuestions() {
+    return _getQuestions();
+}
+
+export function saveQuestion(question) {
+    return _saveQuestion(question);
+}
+
+export function saveQuestionAnswer({ authedUser, qid, answer }) {
+    return _saveQuestionAnswer({ authedUser, qid, answer });
+}
