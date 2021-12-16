888051e9065d0b7b90db17b7b5038e05bca1fe7d
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2021-12-16 15:08:18 +0200

    fix: delete Nav component & nav through App component

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,7 @@ import { handleInitData } from '../actions/shared';
 import Quest from './Quest';
 import Leaderboard from './Leaderboard';
 import QuestNew from './QuestNew';
-import Nav from './Nav';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 class App extends Component {
 
@@ -16,15 +15,34 @@ class App extends Component {
         dispatch(handleInitData());
     }
 
+    handleLogout = e => {
+        e.preventDefault();
+
+        // todo: update authedUser in the store
+        // todo: redirect to login page
+    }
+
 
     render() {
-        const { loading } = this.props;
+        const { loading, authedUserName } = this.props;
 
         return (
             <BrowserRouter>
                 <Fragment>
                     <LoadingBar />
-                    <Nav />
+                    <div className="nav">
+                        <NavLink exact to="/">Home</NavLink>
+                        <NavLink to="/add">New Question</NavLink>
+                        <NavLink to="/leaderboard">Leaderboard</NavLink>
+                        {
+                            loading === true
+                                ? null
+                                : <div className="authed-user">
+                                    <p>Hello {authedUserName},</p>
+                                    <button className="logout" onClick={this.handleLogout}>Logout</button>
+                                </div>
+                        }
+                    </div>
                     <div className="container">
                         {loading === true
                             ? null
@@ -42,10 +60,15 @@ class App extends Component {
     }
 }
 
-function mapStateToProps({ authedUser }) {
+function mapStateToProps({ authedUser, users }) {
+    const authedUserName = authedUser === null 
+        ? null
+        : users[authedUser].name;
+    
     return {
         // todo: loading should be finished just after loading init data (users & questions)
-        loading: authedUser === null
+        loading: authedUser === null,
+        authedUserName,
     }
 }
 
