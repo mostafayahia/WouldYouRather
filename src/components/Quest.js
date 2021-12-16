eb35a534ab0c0bed8c9fdfee7f296e802dff3c9c
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2021-12-16 18:24:28 +0200

    feat: handle question not found case

--- src/components/Quest.js
+++ src/components/Quest.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuestDetailsPreview from './QuestDetailsPreview';
-import { PREVIEW, FULL } from '../utils/quest_details_types';
+import { PREVIEW } from '../utils/quest_details_types';
 import QuestDetailsFull from './QuestDetailsFull';
+import QuestNotFound from './QuestNotFound'
 
 class Quest extends Component {
     render() {
         const { user, quest, detailsType } = this.props;
         console.log('detailstype', detailsType);
 
-        return (
-            <div className="quest">
-                <div className="quest-header">
+        return quest
+            ? <div className="quest">
+                < div className="quest-header" >
                     <p>{user.name} asked:</p>
-                </div>
+                </div >
                 <div className="quest-body">
                     <img src={user.avatarURL}
                         className="avatar"
@@ -28,23 +29,24 @@ class Quest extends Component {
                     </div>
 
                 </div>
-            </div>
-        )
+            </div >
+            : <QuestNotFound />;
+
     }
 }
 
 function mapStateToProps({ questions, users }, props) {
-    console.log('props', props);
 
     const { detailsType } = props;
     const id = props.match.params.id;
-    
+
     const quest = questions[id];
+    const user = quest ? users[quest.author] : null;
 
     return {
         quest,
         detailsType,
-        user: users[quest.author]
+        user,
     };
 }
 
